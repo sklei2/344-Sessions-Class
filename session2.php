826cc5b04deb27204fe24d19122ea5e3a381e17d
Sean Klei <stk1151@rit.edu>
2018-04-10 00:16:09 -0400

    made it so we put the text in a h2

--- session2.php
+++ session2.php
@@ -13,7 +13,7 @@ if ($_SESSION["username"] != "") {
 ?>
 	<h2>Hello <?php echo $_SESSION["username"]; ?></h2>
 	<?php if($_SESSION['name'] != "") { ?>
-		(also known as <?php echo $_SESSION['name']?>)
+		<h2>(also known as <?php echo $_SESSION['name']?>)</h2>
 
 	<?php } ?>
 	<a href="session3.php"> Yet another page </a>
