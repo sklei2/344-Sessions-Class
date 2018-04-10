8186b358b9b428eeea53800c30e2592af65f0905
Sean Klei <stk1151@rit.edu>
2018-04-10 00:04:44 -0400

    i think i added a form post to add a name?

--- session1.php
+++ session1.php
@@ -8,18 +8,34 @@ session_start();
 
 <?php
 
+if (isset($_POST['submit-name']) && !empty($_POST['name'])) {
+	$_SESSION['name'] = $_POST['name'];
+}
+
+
 // Read the sessions
 if ($_SESSION["username"] != "") {
 	echo "<br>Hello ";
     echo $_SESSION["username"];
 ?>
-
+<?php
+if ($_SESSION["name"] != "") {
+	echo "<br> Name: ";
+	echo $_SESSION["name"];
+}
+?>
 <h1>Valid User</h1>
 You are a valid user. Here is your reward!!!<br />
 
 <img src="https://az616578.vo.msecnd.net/files/2015/09/27/635789781274697208191557841_tumblr_lxcavjCQC81qcj7k0o5_r1_250-1.gif">
 
-
+<form class="name-input" role="form" action="<?php ?>"
+	<label> What is your name? </label>	
+	<input type="text" name="name">
+	<button class="btn btn-lg btn-primary btn-block" type="submit" name="submit-name">
+		Submit
+	</button>
+</form>
 <br /><br />
 <a href="clearsessions.php">Clear Sessions</a>
 
