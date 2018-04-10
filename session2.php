ac1e847453a59b2a00456d393562152da743a2bd
Sean Klei <stk1151@rit.edu>
2018-04-10 00:15:25 -0400

    display the name in session 2

--- session2.php
+++ session2.php
@@ -11,7 +11,11 @@ session_start();
 // Read the sessions
 if ($_SESSION["username"] != "") {
 ?>
-	<h2>Hello <?php echo $_SESSION["username"]; ?> </h2>
+	<h2>Hello <?php echo $_SESSION["username"]; ?></h2>
+	<?php if($_SESSION['name'] != "") { ?>
+		(also known as <?php echo $_SESSION['name']?>)
+
+	<?php } ?>
 	<a href="session3.php"> Yet another page </a>
 <?php
 }else{
